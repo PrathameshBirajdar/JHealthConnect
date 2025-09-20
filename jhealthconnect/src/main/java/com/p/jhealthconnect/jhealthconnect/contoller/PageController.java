1dd86126c5597900bab859852117adcc8fedc634
PrathameshBirajdar <prathameshbirajdar.git@gmail.com>
2025-09-20 11:52:18 +0530

    removed

---
+++ jhealthconnect/src/main/java/com/p/jhealthconnect/jhealthconnect/contoller/PageController.java
@@ -0,0 +1,28 @@
+package com.p.jhealthconnect.jhealthconnect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // loads templates/index.html
+    }
+
+    @GetMapping("/doctors")
+    public String doctors() {
+        return "Doctor"; // loads templates/Doctor.html
+    }
+
+    @GetMapping("/appointments")
+    public String appointments() {
+        return "Appointment"; // loads templates/Appointment.html
+    }
+
+    @GetMapping("/insurance")
+    public String insurance() {
+        return "Insurance"; // loads templates/Insurance.html
+    }
+}
